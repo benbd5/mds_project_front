44575f2fcc0b3e62e57a98c2daa5a4669859111b
Benjamin Belz <belzbenjaminp@gmail.com>
2022-01-16 19:33:12 +0100

    Récupérer et afficher les informations de l'utilisateur ainsi que les sessions qu'il a créé. Lui permettre de les modifier et les supprimer

--- src/components/Auth/Profile.js
+++ src/components/Auth/Profile.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { deleteSession } from '../../services/api'
 
 export default function Profile ({ logout, userProfile }) {
-  return (
-    <div>
-      <h2>Vous êtes connecté</h2>
-      <button onClick={logout}>Se déconnecter</button>
-      <p>{userProfile && JSON.stringify(userProfile)}</p>
-    </div>
-  )
+  const navigate = useNavigate()
+
+  if (userProfile[0] && userProfile[1]) {
+    const sessionsOfUser = userProfile[1].map((item, i) => {
+      const handleDelete = async () => {
+        if (window.confirm('Voulez-vous vraiment supprimer ?')) {
+          await deleteSession(item._id)
+          await navigate('/profile')
+        }
+      }
+      return (
+        <div key={item._id}>
+          <p>Sport : {item.sport}</p>
+          <p>Description : {item.description}</p>
+          <p>Lieu : {item.place}</p>
+          <p>Date : {item.date}</p>
+          <Link to={`/edit-session/${item._id}`}>
+            <button className='btn btn-primary' type='submit'>
+              Modifier
+            </button>
+          </Link>
+          <button className='btn btn-primary' onClick={handleDelete}>
+            Supprimer
+          </button>
+          <hr />
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        <h2>Vous êtes connecté</h2>
+        <button onClick={logout} className='btn btn-primary'>Se déconnecter</button>
+        <div>
+          <h3>Informations concernant votre profile</h3>
+          <p>Prénom : {userProfile[0].firstname}</p>
+          <p>Nom : {userProfile[0].lastname}</p>
+          <p>Email : {userProfile[0].email}</p>
+          <p>Téléphone : {userProfile[0].phone}</p>
+        </div>
+        <div>
+          <h3>Les sessions liées à votre profile</h3>
+          {sessionsOfUser}
+        </div>
+      </div>
+    )
+  } else {
+    return <p>Données en cous de chargement...</p>
+  }
 }
--- src/pages/SessionInfos.js
+++ src/pages/SessionInfos.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import SessionInfos from '../components/Sessions/SessionInfos'
-import { getOneSession, deleteSession } from '../services/api'
+import { getOneSession } from '../services/api'
 
 export default function InfosSession () {
   const [oneSession, setOneSession] = useState([])
   const { id } = useParams()
-  const navigate = useNavigate()
-
   // UseEffect => https://stackoverflow.com/questions/55840294/how-to-fix-missing-dependency-warning-when-using-useeffect-react-hook
   useEffect(() => {
     const getData = async () => {
@@ -18,25 +16,9 @@ export default function InfosSession () {
     getData(id)
   }, [])
 
-  const handleDelete = async () => {
-    if (window.confirm('Voulez-vous vraiment supprimer ?')) {
-      console.log('id infos', id)
-      await deleteSession(id)
-      navigate('/sessions')
-    }
-  }
-
   return (
     <div>
       <SessionInfos oneSession={oneSession} />
-      <Link to={`/edit-session/${id}`}>
-        <button className='btn btn-primary' type='submit'>
-          Modifier
-        </button>
-      </Link>
-      <button className='btn btn-primary' onClick={handleDelete}>
-        Supprimer
-      </button>
     </div>
   )
 }
